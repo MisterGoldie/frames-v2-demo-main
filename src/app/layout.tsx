1d71003e1550f3eae80cb2701da5e5f003146794
MisterGoldie <118089324+MisterGoldie@users.noreply.github.com>
2024-12-10 00:29:59 -0600

    SDDFBSF

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,36 +1,28 @@
 import "~/app/globals.css";
 import { Metadata } from "next";
-import { Providers } from "~/app/providers";
 
 const appUrl = process.env.NEXT_PUBLIC_URL;
 
 export const metadata: Metadata = {
-  title: "POD Play v2",
-  description: "A Farcaster Frames v2 demo app",
+  title: "POD Play Tic-Tac-Toe",
+  description: "A Tic-Tac-Toe game presented by /thepod",
   other: {
     'fc:frame': JSON.stringify({
-      version: 'next',
+      version: 'vNext',
       imageUrl: `${appUrl}/splash.png`,
       buttons: [
         {
-          label: "Play game",
-          action: {
-            type: "post_redirect",
-            url: "https://podplayv2demo.vercel.app/"
-          }
-        },
-        {
-          label: "Open Warpcast",
-          action: {
-            type: "post_redirect",
-            url: "https://warpcast.com/~/compose"
-          }
-        },
-        {
-          label: "Add Frame",
+          label: "Start Game",
           action: {
             type: "post",
-            url: `${appUrl}/api/frame/add`
+            url: `${appUrl}/api/frame/tictactoe`
+          }
+        },
+        {
+          label: "How to Play",
+          action: {
+            type: "post",
+            url: `${appUrl}/api/frame/about`
           }
         }
       ]
@@ -49,10 +41,11 @@ export default function RootLayout({
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
+        <link href="https://fonts.googleapis.com/css2?family=Frijole&display=swap" rel="stylesheet" />
       </head>
       <body className="bg-black flex items-center justify-center min-h-screen">
         <div className="w-full max-w-md px-4">
-          <Providers>{children}</Providers>
+          {children}
         </div>
       </body>
     </html>
